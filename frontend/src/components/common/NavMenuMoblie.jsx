ffbb3d2334bd5c9808149cba7d68aa29d5d6f4b7
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2024-08-03 17:35:01 +0300

    Creating Necessary Components For Home

---
+++ frontend/src/components/common/NavMenuMoblie.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NavMenuMoblie extends Component {
+  render() {
+    return (
+      <div>NavMenuMoblie</div>
+    )
+  }
+}
+
+export default NavMenuMoblie
