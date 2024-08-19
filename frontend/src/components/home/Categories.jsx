b1dff7a2d9e9307b4efb86a22ba8066394a24b6c
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2024-08-19 19:59:03 +0300

    Categories Component

---
+++ frontend/src/components/home/Categories.jsx
@@ -0,0 +1,229 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function Categories() {
+  return (
+    <Container className="text-center" fluid={true}>
+      <div className="section-title text-center mb-55">
+        <h2>Categories </h2>
+        {/* <p>Some Of Our Exclusive Collection , You May Like</p> */}
+      </div>
+      <Row>
+        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+          <Row>
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100"
+                  />
+                  <h5 className="category-name">Groceries </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100"
+                  />
+                  <h5 className="category-name">mobile </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Fashion </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Electronics </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+          <Row>
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Home & Furniture </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0139228b2f7eb413.jpg?q=100"
+                  />
+                  <h5 className="category-name">Appliances </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/71050627a56b4693.png?q=100"
+                  />
+                  <h5 className="category-name">Travel </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Two Wheelers </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+          <Row>
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100"
+                  />
+                  <h5 className="category-name">Groceries </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100"
+                  />
+                  <h5 className="category-name">mobile </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Fashion </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Electronics </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+          <Row>
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Home & Furniture </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0139228b2f7eb413.jpg?q=100"
+                  />
+                  <h5 className="category-name">Appliances </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://rukminim2.flixcart.com/flap/80/80/image/71050627a56b4693.png?q=100"
+                  />
+                  <h5 className="category-name">Travel </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                  <Card.Img
+                    className="center"
+                    src="https://cdn-icons-png.flaticon.com/512/135/135763.png"
+                  />
+                  <h5 className="category-name">Two Wheelers </h5>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Categories;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import FeaturedProducts from "../components/home/FeaturedProducts";
+import Categories from "../components/home/Categories";
 
 export class Home extends Component {
   render() {
     return (
       <>
         <FeaturedProducts />
+        <Categories />
       </>
     );
   }
