d2ddef2a0ced1242cf32e1a5b5ddf0c1f093e828
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2024-09-07 19:44:38 +0300

    Nav Menu Desktop Part 2

--- frontend/src/components/common/NavMenuDesktop.jsx
+++ frontend/src/components/common/NavMenuDesktop.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Logo from "../../assets/images/logo.jpg";
 
 const NavMenuDesktop = () => {
@@ -10,17 +11,38 @@ const NavMenuDesktop = () => {
     <>
       <div className="TopSectionDown">
         <Navbar className="navbar" fixed={"top"}>
-          <Container fluid={true}>
+          <Container
+            fluid={"true"}
+            className="fixed-top shadow-sm p-2 mb-0 bg-white"
+          >
             <Row>
               <Col lg={4} md={4} sm={12} xs={12}>
                 <Link to="/">
-                  <img className="nav-logo" src={Logo} alt="logo"/>
+                  <img className="nav-logo" src={Logo} alt="logo" />
                 </Link>
               </Col>
 
-              <Col lg={4} md={4} sm={12} xs={12}></Col>
+              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                <div className="input-group w-100">
+                  <input type="text" className="form-control" />
+                  <Button type="button" className="btn site-btn">
+                    <i className="fa fa-search"> </i>
+                  </Button>
+                </div>
+              </Col>
 
-              <Col lg={4} md={4} sm={12} xs={12}></Col>
+              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                <Link to="/" className="btn">
+                  <i className="fa h4 fa-bell"></i>
+                  <sup>
+                    <span className="badge text-white bg-danger"> 5</span>
+                  </sup>
+                </Link>
+                <a className="btn" href="test"> <i className="fa h4 fa-mobile-alt"></i></a>
+                <Link to="/" className="h4 btn">Login </Link>
+
+                <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items</Button>
+              </Col>
             </Row>
           </Container>
         </Navbar>
