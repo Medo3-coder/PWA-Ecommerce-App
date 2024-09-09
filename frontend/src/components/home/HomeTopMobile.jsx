80fe021dfcd422544d20839cd05d216e347666ae
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2024-09-09 15:28:29 +0300

    Mobile View Menu Part 4

---
+++ frontend/src/components/home/HomeTopMobile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import HomeSlider from "./HomeSlider";
+
+const HomeTopMobile = () => {
+  return (
+    <>
+      <Container className="p-0 m-0 overflow-hidden" fluid={true}>
+        <Row className="p-0 m-0 overflow-hidden">
+          <Col lg={12} md={12} sm={12}>
+            <HomeSlider />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default HomeTopMobile;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,17 +3,25 @@ import FeaturedProducts from "../components/home/FeaturedProducts";
 import Categories from "../components/home/Categories";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
-import HomeTop from "../components/home/HomeTop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
 import NavMenuMoblie from "../components/common/NavMenuMoblie";
+import HomeTopMobile from "../components/home/HomeTopMobile";
+import HomeTop from "../components/home/HomeTop";
 
 export class Home extends Component {
   render() {
     return (
       <>
-        {/* <NavMenuDesktop /> */}
-        <NavMenuMoblie />
-        <HomeTop />
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMoblie />
+          <HomeTopMobile />
+        </div>
+
         <FeaturedProducts />
         <NewArrival />
         <Categories />
