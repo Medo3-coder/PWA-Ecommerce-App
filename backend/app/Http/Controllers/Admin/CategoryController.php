99ba1899c53f4bb573cacc66409ae44dff678507
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2024-12-03 21:25:49 +0200

    fix

--- backend/app/Http/Controllers/Admin/CategoryController.php
+++ backend/app/Http/Controllers/Admin/CategoryController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
     public function catagories(){
 
-        $categories = Category::with('subcategories')->get();
+        $categories = Cache::remember('categories_with_subcategories', 60, function () {
+            return Category::with('subcategories')->get();
+        });
 
+        if ($categories->isEmpty()) {
+            return response()->json(['message' => 'No categories available now'], 404);
+        }
         return response()->json($categories , 200);
 
     }
