74b745f9657cde9f2f186c81352b7713c443b02f
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2025-01-23 15:59:24 +0200

    use useQuery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './routes/AppRoutes';
 
+
+const queryClient = new QueryClient();
+
+
 function App() {
-  return (
-     <Fragment>
-         <Router>
-           <AppRoutes />
-        </Router>
-        <ToastContainer />
-     </Fragment>
-  );
+   return (
+      <Fragment>
+         {/* Wrap your app with QueryClientProvider and pass the queryClient */}
+         <QueryClientProvider client={queryClient}>
+            <Router>
+               <AppRoutes />
+            </Router>
+            <ToastContainer />
+         </QueryClientProvider>
+      </Fragment>
+   );
 }
 
 export default App;
--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, {/* useEffect, useState */} from "react";
+import { useQuery } from 'react-query';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -12,30 +13,35 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 function Categories() {
-  const [menuData, setMenuData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null); // State for errors
 
-  useEffect(() => {
-    const categories = async () => {
-      try {
-        const response = await axios.get(AppURL.CategoryDetails);
-        if (response.status === 200) {
-          setMenuData(response.data.categories);
-        }
-      } catch (e) {
-        setError(
-          ToastMessages.showError(
-            "Failed to load Category Information in page section."
-          )
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
+  const { data, error, isLoading } = useQuery('categories', async () => {
+    const response = await axios.get(AppURL.CategoryDetails);
+    return response.data.categories;
+  });
+  // const [menuData, setMenuData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null); // State for errors
 
-    categories();
-  }, []); // Empty dependency array to run this effect only once on mount
+  // useEffect(() => {
+  //   const categories = async () => {
+  //     try {
+  //       const response = await axios.get(AppURL.CategoryDetails);
+  //       if (response.status === 200) {
+  //         setMenuData(response.data.categories);
+  //       }
+  //     } catch (e) {
+  //       setError(
+  //         ToastMessages.showError(
+  //           "Failed to load Category Information in page section."
+  //         )
+  //       );
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   categories();
+  // }, []); // Empty dependency array to run this effect only once on mount
 
   const renderSkeletons = Array.from({ length: 12 }).map((_, index) => (
     <Col key={index} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
@@ -48,7 +54,7 @@ function Categories() {
     </Col>
   ));
 
-  if (loading) {
+  if (isLoading) {
     return (
       <Container className="text-center" fluid={true}>
         <div className="section-title text-center mb-55">
@@ -62,12 +68,12 @@ function Categories() {
   if (error) {
     return (
       <Container className="text-center">
-        <h4>{error}</h4>
+        <h4>{ToastMessages.showError(error)}</h4>
       </Container>
     );
   }
 
-  const MyView = menuData.map((category, index) => (
+  const MyView = data.map((category, index) => (
     <Col key={index} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
       <Link className="text-link" to={`/${category.slug}`}>
         <Card className="h-100 w-100 text-center">
