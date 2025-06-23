81780dd55bf31dc82942bd01479480f6aabbd28b
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2025-06-23 23:30:22 +0300

    login

--- backend/resources/views/admin/auth/login.blade.php
+++ backend/resources/views/admin/auth/login.blade.php
@@ -2,81 +2,96 @@
 <html lang="en">
 
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--favicon-->
-	<link rel="icon" href="{{asset('admin/assets/images/favicon-32x32.png')}}" type="image/png" />
-	<!--plugins-->
-	<link href="{{asset('admin/assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
-	<link href="{{asset('admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
-	<link href="{{asset('admin/assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
-	<!-- loader-->
-	<link href="{{asset('admin/assets/css/pace.min.css')}}" rel="stylesheet" />
-	<script src="{{asset('admin/assets/js/pace.min.js')}}"></script>
-	<!-- Bootstrap CSS -->
-	<link href="{{asset('admin/assets/css/bootstrap.min.css')}}" rel="stylesheet">
-	<link href="{{asset('admin/assets/css/app.css')}}" rel="stylesheet">
-	<link href="{{asset('admin/assets/css/icons.css')}}" rel="stylesheet">
-	<!-- SweetAlert2 CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
-	<title>Rukada - Responsive Bootstrap 5 Admin Template</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('admin/assets/images/favicon-32x32.png') }}" type="image/png" />
+    <!--plugins-->
+    <link href="{{ asset('admin/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+    <!-- loader-->
+    <link href="{{ asset('admin/assets/css/pace.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('admin/assets/js/pace.min.js') }}"></script>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/css/icons.css') }}" rel="stylesheet">
+    <!-- SweetAlert2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+    <title>Rukada - Responsive Bootstrap 5 Admin Template</title>
 </head>
 
 <body class="bg-login">
-	<!--wrapper-->
-	<div class="wrapper">
-		<div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
-			<div class="container-fluid">
-				<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-					<div class="col mx-auto">
-						<div class="mb-4 text-center">
-							{{-- <img src="{{asset('admin/assets/images/logo-img.png')}}" width="180" alt="" /> --}}
-						</div>
-						<div class="card">
-							<div class="card-body">
-								<div class="border p-4 rounded">
-									<div class="text-center">
-										<h3 class="">Sign in</h3>
-									</div>
+    <!--wrapper-->
+    <div class="wrapper">
+        <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+            <div class="container-fluid">
+                <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+                    <div class="col mx-auto">
+                        <div class="mb-4 text-center">
+                            {{-- <img src="{{asset('admin/assets/images/logo-img.png')}}" width="180" alt="" /> --}}
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="border p-4 rounded">
                                     <div class="text-center">
-										<hr/>
-									</div>
-									<div class="form-body">
-										<form class="row g-3 needs-validation" method="POST" action="{{ route('admin.login') }}" novalidate>
+                                        <h3 class="">Sign in</h3>
+                                    </div>
+                                    <div class="text-center">
+                                        <hr />
+                                    </div>
+                                    <div class="form-body">
+                                        <form class="row g-3 needs-validation" method="POST"
+                                            action="{{ route('admin.login') }}" novalidate>
                                             @csrf
                                             <div class="col-12">
                                                 <label for="inputEmailAddress" class="form-label">Email Address</label>
 
-                                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="inputEmailAddress" name="email" placeholder="Email Address" required value="{{ old('email') }}">
-                                                       @error('email')
-                                                <div class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
-                                                    <div class="text-white">{{ $message }}</div>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                                </div>
+                                                <input type="email"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    id="inputEmailAddress" name="email" placeholder="Email Address"
+                                                    required value="{{ old('email') }}">
+                                                @error('email')
+                                                    <div
+                                                        class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
+                                                        <div class="text-white">{{ $message }}</div>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                            aria-label="Close"></button>
+                                                    </div>
                                                 @enderror
                                                 {{-- <div class="invalid-feedback">Please enter a valid email address.</div> --}}
                                             </div>
                                             <div class="col-12">
-                                                <label for="inputChoosePassword" class="form-label">Enter Password</label>
+                                                <label for="inputChoosePassword" class="form-label">Enter
+                                                    Password</label>
 
                                                 <div class="input-group" id="show_hide_password">
-                                                    <input type="password" class="form-control border-end-0 @error('password') is-invalid @enderror" id="inputChoosePassword" name="password" placeholder="Enter Password" required minlength="6">
-                                                    <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+                                                    <input type="password"
+                                                        class="form-control border-end-0 @error('password') is-invalid @enderror"
+                                                        id="inputChoosePassword" name="password"
+                                                        placeholder="Enter Password" required minlength="6">
+                                                    <a href="javascript:;" class="input-group-text bg-transparent"><i
+                                                            class='bx bx-hide'></i></a>
                                                 </div>
 
-                                                         @error('password')
-                                                <div class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
-                                                    <div class="text-white">{{ $message }}</div>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                                </div>
+                                                @error('password')
+                                                    <div
+                                                        class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
+                                                        <div class="text-white">{{ $message }}</div>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                            aria-label="Close"></button>
+                                                    </div>
                                                 @enderror
                                                 {{-- <div class="invalid-feedback">Password must be at least 6 characters.</div> --}}
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="form-check form-switch">
-                                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name="remember">
-                                                    <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                                                    <input class="form-check-input" type="checkbox"
+                                                        id="flexSwitchCheckChecked" name="remember">
+                                                    <label class="form-check-label"
+                                                        for="flexSwitchCheckChecked">Remember Me</label>
                                                 </div>
                                             </div>
                                             <div class="col-md-6 text-end">
@@ -84,66 +99,67 @@
                                             </div>
                                             <div class="col-12">
                                                 <div class="d-grid">
-                                                    <button type="submit" class="btn btn-primary"><i class="bx bxs-lock-open"></i>Sign in</button>
+                                                    <button type="submit" class="btn btn-primary"><i
+                                                            class="bx bxs-lock-open"></i>Sign in</button>
                                                 </div>
                                             </div>
                                         </form>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--end row-->
-			</div>
-		</div>
-	</div>
-	<!--end wrapper-->
-	<!-- Bootstrap JS -->
-	<script src="{{asset('admin/assets/js/bootstrap.bundle.min.js')}}"></script>
-	<!--plugins-->
-	<script src="{{asset('admin/assets/js/jquery.min.js')}}"></script>
-	<script src="{{asset('admin/assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
-	<script src="{{asset('admin/assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
-	<script src="{{asset('admin/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
-	<!-- SweetAlert2 JS -->
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<!--Password show & hide js -->
-	<script>
-		$(document).ready(function () {
-			$("#show_hide_password a").on('click', function (event) {
-				event.preventDefault();
-				if ($('#show_hide_password input').attr("type") == "text") {
-					$('#show_hide_password input').attr('type', 'password');
-					$('#show_hide_password i').addClass("bx-hide");
-					$('#show_hide_password i').removeClass("bx-show");
-				} else if ($('#show_hide_password input').attr("type") == "password") {
-					$('#show_hide_password input').attr('type', 'text');
-					$('#show_hide_password i').removeClass("bx-hide");
-					$('#show_hide_password i').addClass("bx-show");
-				}
-			});
-		});
-	</script>
-	<script>
-		// Bootstrap validation
-		(function () {
-  'use strict'
-  var forms = document.querySelectorAll('.needs-validation')
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-	</script>
-	<!--app JS-->
-	<script src="{{asset('admin/assets/js/app.js')}}"></script>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end row-->
+            </div>
+        </div>
+    </div>
+    <!--end wrapper-->
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!--plugins-->
+    <script src="{{ asset('admin/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <!-- SweetAlert2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!--Password show & hide js -->
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bx-hide");
+                    $('#show_hide_password i').removeClass("bx-show");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bx-hide");
+                    $('#show_hide_password i').addClass("bx-show");
+                }
+            });
+        });
+    </script>
+    <script>
+        // Bootstrap validation
+        (function() {
+            'use strict'
+            var forms = document.querySelectorAll('.needs-validation')
+            Array.prototype.slice.call(forms)
+                .forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+    </script>
+    <!--app JS-->
+    <script src="{{ asset('admin/assets/js/app.js') }}"></script>
 </body>
 
 </html>
